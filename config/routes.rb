a101440343da0c9e5f51535ffd5b5614e9476965
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-10-31 13:34:02 +0430

    bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   get '/users', to: 'users#index'
-  get '/users/:user_id', to: 'users#show', as: 'user'
+  get '/users/:id', to: 'users#show', as: 'user'
   get '/users/:user_id/posts', to: 'posts#index', as: 'user_posts'
-  get '/users/:user_id/posts/:post_id', to: 'posts#show', as: 'user_post'
+  get '/users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
 end
