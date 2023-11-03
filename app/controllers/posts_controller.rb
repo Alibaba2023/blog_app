25b59102e9bd231a895ad46538d0ab294754c9d3
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-11-03 09:22:12 +0430

    view added for posts and users

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
-  def show; end
+  def index
+    @user = User.find(params[:user_id])
+    @posts = @user.posts
+  end
 
-  def index; end
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+  end
 end
