a1f372dfbcd0f4b8d5436260524be82ea1dd6c2f
Alibaba2023 <126192888+Alibaba2023@users.noreply.github.com>
2023-11-07 01:06:23 +0430

    fixed the N+1 problems

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
   def create
     @current_user = current_user
-    @post = Post.find(params[:post_id])
+    @post = Post.includes(comments: :user).find(params[:post_id])
     @comment = Comment.new(user_id: @current_user.id, post_id: @post.id, text: params[:comment])
     return unless @comment.save
 
-    redirect_to "/users/#{@current_user.id}/posts/#{@post.id}"
+    redirect_to user_post_path(@current_user, @post)
   end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,6 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(user_id: current_user.id, post_id: params[:post_id])
     @like.save
-    redirect_to "/users/#{current_user.id}/posts/#{params[:post_id]}"
+    redirect_to user_post_path(current_user, params[:post_id])
   end
 end
